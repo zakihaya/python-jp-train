72f67a37034eec5844d5384284bb4b178b383424
hayazaki <toru.hayazaki@gmail.com>
2021-10-13 14:29:39 +0900

    辞書

---
+++ 09-辞書/dictionary.py
@@ -0,0 +1,14 @@
+english_words = { "apple": "りんご", "peach": "もも", "orange": "みかん" }
+print(english_words)
+print(english_words["peach"])
+
+dict_obj = {}
+dict_obj["dog"] = "犬"
+dict_obj["cat"] = "猫"
+print(dict_obj["dog"])
+
+del dict_obj["dog"]
+print(len(dict_obj))
+print(dict_obj)
+print(len(dict_obj))
+
